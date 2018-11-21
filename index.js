c5a7aed2335c8ef51f8ae674e4aed05e3bc6c74e
zzhdong <zzhdong@gmail.com>
2018-11-21 22:03:17 +0800

    Add ColorPicker

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-
-import { NativeModules } from 'react-native';
-
 //颜色选择器
-export { default as SlidersColorPicker } from 'src/colorPicker/pickers/SlidersColorPicker';
+export { SlidersColorPicker } from './src/colorPicker/pickers/SlidersColorPicker';
