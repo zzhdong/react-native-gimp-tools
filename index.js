d9ef9ad0226be4b9a7c7f48c50aa468b23b8162a
zzhdong <zzhdong@gmail.com>
2018-11-23 14:57:55 +0800

    Remove Shadow

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import ToastNative from './src/toast/ToastNative';
 import SvgUri from './src/svg/SvgUri';
-import BoxShadow from './src/shadow/BoxShadow';
-import BorderShadow from './src/shadow/BorderShadow';
 import SlidersColorPicker from './src/colorPicker/pickers/SlidersColorPicker';
 
 export {
     ToastNative,                    //Toast
     SvgUri,                         //SvgUri
-    BoxShadow,                      //shadow
-    BorderShadow,                   //shadow
     SlidersColorPicker              //颜色选择器
 };
